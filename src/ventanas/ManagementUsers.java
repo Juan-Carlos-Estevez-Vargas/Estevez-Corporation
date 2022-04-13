92c03db83eb9986f3182ce56e7823330e42d3e47
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-04-13 16:22:37 -0500

    project updated and code commented.

--- src/ventanas/ManagementUsers.java
+++ src/ventanas/ManagementUsers.java
@@ -19,15 +19,27 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import modelo.DatabaseConnection;
 
+/**
+ * 
+ * @author Juan Carlos Estevez Vargas.
+ *
+ */
 public class ManagementUsers extends JFrame implements ActionListener {
 
+	/**
+	 * Declaración de Variables.
+	 */
+	private static final long serialVersionUID = 1L;
 	private JLabel jlabel1;
 	private JPanel container;
 	private JTable tableUsers;
 	public static String user_update = "";
 	private JScrollPane scrollPaneUsers;
-	DefaultTableModel model = new DefaultTableModel();
-	
+	private DefaultTableModel model = new DefaultTableModel();
+
+	/**
+	 * Constructor de Clase.
+	 */
 	public ManagementUsers() {
 		this.setSize(630, 340);
 		this.setResizable(false);
@@ -36,72 +48,84 @@ public class ManagementUsers extends JFrame implements ActionListener {
 		this.setLayout(null);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.initComponents();
-		
-		//Hacemos la conexion a la base de datos
-        try {
-            Connection cn = DatabaseConnection.conectar();
-            PreparedStatement pst = cn.prepareStatement(
-                    "SELECT id_usuario, nombre_usuario, username, tipo_nivel, estatus FROM usuarios");
 
-            ResultSet rs = pst.executeQuery();
+		/**
+		 * Conexion a la base de datos ya que se deben listar los usuarios tan pronto se
+		 * inicie su ejecución.
+		 */
+		try {
+			Connection cn = DatabaseConnection.conectar();
+			PreparedStatement pst = cn
+					.prepareStatement("SELECT id_usuario, nombre_usuario, username, tipo_nivel, estatus FROM usuarios");
+			ResultSet rs = pst.executeQuery();
 
-            //Creamos la tabla y la añadimos al jScrollPanel
-            this.tableUsers = new JTable(model);
-    		this.scrollPaneUsers = new JScrollPane(this.tableUsers);
-    		this.scrollPaneUsers.setBounds(25,55,540,200);
-    		
+			/**
+			 * Creamos la tabla y la añadimos al JScrollPanel.
+			 */
+			this.tableUsers = new JTable(model);
+			this.scrollPaneUsers = new JScrollPane(this.tableUsers);
+			this.scrollPaneUsers.setBounds(25, 55, 540, 200);
 
-            //Añadimos las columnas a la tabla
-            model.addColumn(" ");
-            model.addColumn("Nombre Usuario");
-            model.addColumn("Username");
-            model.addColumn("Permisos");
-            model.addColumn("Estatus");
+			/**
+			 * Añadiendo las columnas a la tabla.
+			 */
+			model.addColumn(" ");
+			model.addColumn("Nombre Usuario");
+			model.addColumn("Username");
+			model.addColumn("Permisos");
+			model.addColumn("Estatus");
 
-            //Llenado de la tabla
-            while (rs.next()) { //Cada que la base de datos encuentre resultados
-                Object[] fila = new Object[5];
+			/**
+			 * Llenado de la tabla
+			 */
+			while (rs.next()) {
+				Object[] row = new Object[5];
+				for (int i = 0; i < 5; i++) {
+					row[i] = rs.getObject(i + 1);
+				}
+				model.addRow(row);
+			}
+			cn.close();
+		} catch (SQLException e) {
+			System.err.println("Error al llenar Tabla " + e);
+			JOptionPane.showMessageDialog(null, "¡¡Error al mostrar informacion!! Contacte al Administrador");
+		}
 
-                for (int i = 0; i < 5; i++) {
-                    fila[i] = rs.getObject(i + 1);
-                }
+		/**
+		 * Eventos de la tabla para mostrar informacion de los usuario
+		 */
+		tableUsers.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int row_point = tableUsers.rowAtPoint(e.getPoint()); // Esta variable almacena la fila seleccionada
+				int column_point = 2;
 
-                model.addRow(fila);
-            }
-
-            cn.close();
-
-        } catch (SQLException e) {
-            System.err.println("Error al llenar Tabla " + e);
-            JOptionPane.showMessageDialog(null, "¡¡Error al mostrar informacion!! Contacte al Administrador");
-        }
-        
-      //Eventos de la tabla para mostrar informacion de los usuario
-        tableUsers.addMouseListener(new MouseAdapter() {
-
-            @Override
-            public void mouseClicked(MouseEvent e) {
-
-                int fila_point = tableUsers.rowAtPoint(e.getPoint()); //Esta variable almacena la fila seleccionada
-                int columna_point = 2; //Esta variable almacena la columna 2
-
-                if (fila_point > -1) {
-                    user_update = (String) model.getValueAt(fila_point, columna_point);
-                    UserInformation userInformation = new UserInformation();
-                    userInformation.setVisible(true);
-                }
-            }
-        });
-        
-        this.container.add(scrollPaneUsers);
+				if (row_point > -1) {
+					user_update = (String) model.getValueAt(row_point, column_point);
+					UserInformation userInformation = new UserInformation();
+					userInformation.setVisible(true);
+				}
+			}
+		});
+		this.container.add(scrollPaneUsers);
 	}
 
+	/**
+	 * Construye los componentes Swing en el Frame.
+	 */
 	public void initComponents() {
+
+		/**
+		 * Panel Principal.
+		 */
 		this.container = new JPanel();
 		this.container.setBackground(new Color(46, 59, 104));
 		this.container.setLayout(null);
 		this.setContentPane(container);
 
+		/**
+		 * Título del Panel.
+		 */
 		this.jlabel1 = new JLabel("Usuarios Registrados");
 		this.jlabel1.setBounds(210, 10, 250, 20);
 		this.jlabel1.setForeground(new Color(192, 192, 192));
