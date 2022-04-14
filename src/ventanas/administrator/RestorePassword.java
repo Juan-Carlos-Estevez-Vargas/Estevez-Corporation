d78eefae9d3757c7db8b9cb6440b94fedcaf1916
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2022-04-14 12:57:29 -0500

    administrator module finished.

--- src/ventanas/administrator/RestorePassword.java
+++ src/ventanas/administrator/RestorePassword.java
@@ -3,23 +3,25 @@ package ventanas.administrator;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.PreparedStatement;
-
 import modelo.DatabaseConnection;
 
+/**
+ * 
+ * @author Juan Carlos Estevez Vargas.
+ *
+ */
 public class RestorePassword extends JFrame implements ActionListener{
 
 	/**
-	 * 
+	 * Declaración de Variables.
 	 */
 	private static final long serialVersionUID = 1L;
 	String user = "", user_update = "";
@@ -58,23 +60,22 @@ public class RestorePassword extends JFrame implements ActionListener{
 		/**
 		 * Label Principal.
 		 */
-		labelTittle = new JLabel();
-		labelTittle.setFont(new java.awt.Font("Segoe UI", 0, 24));
+		labelTittle = new JLabel("Cambio de Password");
+		labelTittle.setFont(new java.awt.Font("Segoe UI", 0, 22));
 		labelTittle.setForeground(new java.awt.Color(192, 192, 192));
 		labelTittle.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-		labelTittle.setText("Cambio de Password");
-		labelTittle.setBounds(70, 10, 200, 30);
-		this.container.add(labelTittle);
+		labelTittle.setBounds(50, 10, 300, 30);
+		this.container.add(labelTittle);	
 		
 		/**
 		 * Label New Password.
 		 */
 		labelNewPassword = new JLabel();
-		labelNewPassword.setFont(new java.awt.Font("Segoe UI", 0, 24));
+		labelNewPassword.setFont(new java.awt.Font("Segoe UI", 0, 18));
 		labelNewPassword.setForeground(new java.awt.Color(192, 192, 192));
-		labelNewPassword.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+		labelNewPassword.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
 		labelNewPassword.setText("Nuevo Password");
-		labelNewPassword.setBounds(20, 50, 200, 30);
+		labelNewPassword.setBounds(20, 50, 200, 20);
 		this.container.add(labelNewPassword);
 		
 		/**
@@ -83,17 +84,18 @@ public class RestorePassword extends JFrame implements ActionListener{
 		txtNewPassword = new JTextField();
 		txtNewPassword.setFont(new java.awt.Font("Segoe UI", 1, 16));
 		txtNewPassword.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-		txtNewPassword.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-		txtNewPassword.setBounds(20, 70, 300, 20);
+		txtNewPassword.setBounds(20, 70, 300, 30);
+		this.txtNewPassword.setBackground(new Color(127, 140, 141));
+		this.txtNewPassword.setForeground(Color.WHITE);
 		this.container.add(txtNewPassword);
 		
 		/**
 		 * Label Confirm Password.
 		 */
 		labelConfirmPassword = new JLabel();
-		labelConfirmPassword.setFont(new java.awt.Font("Segoe UI", 0, 24));
+		labelConfirmPassword.setFont(new java.awt.Font("Segoe UI", 0, 18));
 		labelConfirmPassword.setForeground(new java.awt.Color(192, 192, 192));
-		labelConfirmPassword.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+		labelConfirmPassword.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
 		labelConfirmPassword.setText("Confirmar Password");
 		labelConfirmPassword.setBounds(20, 110, 200, 20);
 		this.container.add(labelConfirmPassword);
@@ -104,19 +106,21 @@ public class RestorePassword extends JFrame implements ActionListener{
 		txtConfirmPassword = new JTextField();
 		txtConfirmPassword.setFont(new java.awt.Font("Segoe UI", 1, 16));
 		txtConfirmPassword.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-		txtConfirmPassword.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
 		txtConfirmPassword.setBounds(20, 130, 300, 30);
+		this.txtConfirmPassword.setBackground(new Color(127, 140, 141));
+		this.txtConfirmPassword.setForeground(Color.WHITE);
 		this.container.add(txtConfirmPassword);
 		
 		/**
 		 * Botón para restaurar la contraseña del usuario en cuestión.
 		 */
-		btnRestorePassword = new JButton();
+		btnRestorePassword = new JButton("Restaurar Password");
 		btnRestorePassword.setFont(new java.awt.Font("Tahoma", 1, 18));
-		btnRestorePassword.setText("Restaurar Password");
 		btnRestorePassword.setBorder(null);
 		btnRestorePassword.addActionListener(this);
 		btnRestorePassword.setBounds(20, 180, 300, 35);
+		this.btnRestorePassword.setBackground(new Color(8, 85, 224));
+		this.btnRestorePassword.setForeground(Color.WHITE);
 		this.container.add(btnRestorePassword);
     }
 
